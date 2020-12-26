1a91a87000c7bc332c7453fdf9db2240a2e6c117
EC2 Default User <ec2-user@ip-172-31-35-0.ap-northeast-2.compute.internal>
2020-12-26 17:43:14 +0000

    Allow all hosts && Set the debug option to false

--- instagram_backend/settings.py
+++ instagram_backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'eunv%-gbh6y^^=9e$28ul3t5tb4(z+kv@d5+o*^yh4(&os-)8x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
